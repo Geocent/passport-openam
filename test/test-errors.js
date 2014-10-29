9475a3c800fdd0a7f4ee2c3e5959c23c7eafd613
Joshua Penton <joshua.penton@geocent.com>
2014-10-29 12:27:39 -0500

    Initial test framework.

---
+++ test/test-errors.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var assert = require('assert'),
+    InternalOpenAmError = require('..').InternalOpenAmError;
+
+describe('InternalOpenAmError tests.', function () {
+  it('when constructed with only a message', function () {
+    var err = new InternalOpenAmError('oops');
+    assert.equal(err.toString(), 'oops');
+  });
+
+  it('when constructed with a message and error', function () {
+    var err = new InternalOpenAmError('oops', new Error('something is wrong'));
+    assert.equal(err.toString(), 'oops (Error: something is wrong)');
+  });
+
+  it('when constructed with a message and error', function () {
+    var err = new InternalOpenAmError('oops', { statusCode: 401, data: 'invalid OAuth credentials' });
+    assert.equal(err.toString(), 'oops (status: 401 data: invalid OAuth credentials)');
+  });
+});
---
+++ test/test-module.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert'),
+    OpenAm = require('..');
+
+describe('passport-openam module tests.', function () {
+  it('version test', function () {
+    assert.equal(typeof(OpenAm.version), 'string');
+  });
+});
